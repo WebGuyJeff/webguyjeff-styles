286f2441c7b7ab2d319fb8fac6622d5ab27d2547
BigupJeff <me@jeffersonreal.uk>
2025-01-04 15:17:37 +0000

    split css into files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import "./styles.scss";
+import "./_fonts.scss";
+import "./_variables.scss";
+import "./_global.scss";
+import "./_elements.scss";
+import "./_typography.scss";
+import "./_layout.scss";
+import "./_buttons.scss";
+import "./_menus.scss";
