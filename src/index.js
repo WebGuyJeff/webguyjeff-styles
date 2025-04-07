0b804a9fdbb072950487ef842c7d315732404799
wordpressjeff <me@jeffersonreal.com>
2025-04-07 16:40:24 +0100

    layout improvement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./base/_index";
+import "./parts/_index";
 import "./elements/_index";
 import "./blocks/_index";
 import "./layout/_index";
--- src/layout/_index.js
+++ src/layout/_index.js
@@ -1,7 +1,5 @@
-import "./_body.scss";
 import "./_content-width.scss";
-import "./_global-padding.scss";
+import "./_global-page-padding.scss";
 import "./_align.scss";
-import "./_header.scss";
-import "./_footer.scss";
-import "./_sidebar.scss";
+import "./_page.scss";
+import "./_page-sidebar.scss";
